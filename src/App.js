a18e6e011efe6f03f338f19fcdc89fbb166be92c
Aky Mac Donald <kotoko@Kotoko.local>
2022-07-12 20:24:45 -0400

    Final code

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from "react";
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
+
+function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
+  //was handleClick:
+  const switchLight = () => setLight(light === "on" ? "off" : "on");
+
+  return (
+    <div className={`App ${dark}`}>
+      
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton light={light} switchLight={switchLight} />
+        <TextRepeaterButton />
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
